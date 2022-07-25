6382cc517210131ccb557f7284632aaf119a0283
minpaeng <22alswjd@naver.com>
2022-07-25 19:13:17 +0900

    style: Post 클래스 getter 추가

--- src/main/java/com/hanium/gabojago/domain/Post.java
+++ src/main/java/com/hanium/gabojago/domain/Post.java
@@ -1,9 +1,12 @@
 package com.hanium.gabojago.domain;
 
+import lombok.Getter;
+
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Id;
 
+@Getter
 @Entity
 public class Post {
     @Id
