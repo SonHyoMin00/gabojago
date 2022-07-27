670763145ad5f02e951fa039120004edcdbf55fa
권민정 <22alswjd@naver.com>
2022-07-27 23:06:55 +0900

    fix: 자료형 수정

--- src/main/java/com/hanium/gabojago/domain/User.java
+++ src/main/java/com/hanium/gabojago/domain/User.java
@@ -1,12 +1,18 @@
 package com.hanium.gabojago.domain;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.Id;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 
+import javax.persistence.*;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 @Entity
-public class User {
+public class User extends BaseTimeEntity{
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long userId;
 
     @Column
@@ -14,4 +20,13 @@ public class User {
 
     @Column
     private String name;
+
+    @Column Byte age;
+
+    @Builder
+    public User(String email, String name, Byte age) {
+        this.email = email;
+        this.name = name;
+        this.age = age;
+    }
 }
