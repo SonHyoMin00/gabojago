551afc3cd71e134b0d8454c5c5b7d175bd2e1c0b
minpaeng <22alswjd@naver.com>
2022-07-31 00:02:10 +0900

    fix: 핫플레이스 조회 페이징에서 정렬 관련 버그 수정

--- src/main/java/com/hanium/gabojago/controller/SpotController.java
+++ src/main/java/com/hanium/gabojago/controller/SpotController.java
@@ -1,5 +1,6 @@
 package com.hanium.gabojago.controller;
 
+import com.hanium.gabojago.dto.SpotPageResponse;
 import com.hanium.gabojago.dto.SpotResponse;
 import com.hanium.gabojago.service.SpotService;
 import lombok.RequiredArgsConstructor;
@@ -26,27 +27,27 @@ public class SpotController {
     // 조회순 조회
     @GetMapping
     public List<SpotResponse> getHotplacesByViewCnt(
-            @RequestParam(required = false, defaultValue = "0", value = "page")int page,
+            @RequestParam(required = false, defaultValue = "1", value = "page")int page,
             @RequestParam(required = false, defaultValue = "10", value = "size")int size) {
-        return spotService.findHotplaceByViewCnt(page, size);
+        return spotService.findHotplacesByViewCnt(page - 1, size);
     }
 
     // 지역별 순위 조회
     @GetMapping("region/{region}")
     public List<SpotResponse> getHotplacesByRegion(
             @PathVariable String region,
-            @RequestParam(required = false, defaultValue = "0", value = "page")int page,
+            @RequestParam(required = false, defaultValue = "1", value = "page")int page,
             @RequestParam(required = false, defaultValue = "10", value = "size")int size) {
-        return spotService.findHotplacesByRegion(region, page, size);
+        return spotService.findHotplacesByRegion(region, page - 1, size);
     }
 
     // 컨셉별 핫플레이스 조회
     @GetMapping("tag/{tagId}")
-    public List<SpotResponse> getHotplacesByTag(
+    public SpotPageResponse getHotplacesByTag(
             @PathVariable int tagId,
-            @RequestParam(required = false, defaultValue = "0", value = "page")int page,
+            @RequestParam(required = false, defaultValue = "1", value = "page")int page,
             @RequestParam(required = false, defaultValue = "10", value = "size")int size) {
-        return spotService.findHotplaceByTag(tagId, page, size);
+        return spotService.findHotplacesByTag(tagId, page - 1, size);
     }
 
     // 상세정보 조회
--- src/main/java/com/hanium/gabojago/service/SpotService.java
+++ src/main/java/com/hanium/gabojago/service/SpotService.java
@@ -1,20 +1,21 @@
 package com.hanium.gabojago.service;
 
 import com.hanium.gabojago.domain.Spot;
-import com.hanium.gabojago.domain.SpotTag;
+import com.hanium.gabojago.dto.SpotPageResponse;
 import com.hanium.gabojago.dto.SpotResponse;
 import com.hanium.gabojago.repository.SpotRepository;
 import com.hanium.gabojago.repository.SpotTagRepository;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
-import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.stream.Collectors;
 
+@Slf4j
 @RequiredArgsConstructor
 @Service
 public class SpotService {
@@ -28,28 +29,63 @@ public class SpotService {
     }
 
     // 조회순으로 정렬된 핫플레이스 데이터 가져오기
-    public List<SpotResponse> findHotplaceByViewCnt(int page, int size) {
-        Page<Spot> spots = spotRepository.findAll(PageRequest.of(
-                page, size, Sort.by(Sort.Direction.DESC, "viewCnt")));
-        return spots.getContent()
-                .stream().map(SpotResponse::new).collect(Collectors.toList());
+    public  List<SpotResponse> findHotplacesByViewCnt(int page, int size) {
+        List<Spot> spots = spotRepository.findHotplacesByViewCnt(PageRequest.of(page, size));
+
+        return spots.stream().map(SpotResponse::new).collect(Collectors.toList());
+//        //총 페이지 수
+//        int totalPages = spots.getTotalPages();
+//        log.info("총 페이지 수: " + spots.getTotalPages());
+//
+//        //spotResponses DTO로 변환
+//        List<SpotResponse> spotResponses = spots.getContent()
+//                .stream().map(SpotResponse::new).collect(Collectors.toList());
+//
+//        // 총 페이지 수 추가하여 반환
+//        return SpotPageResponse.builder()
+//                .spotResponses(spotResponses)
+//                .totalPages(totalPages)
+//                .build();
     }
 
     // 지역별 핫플레이스 데이터 가져오기
     public List<SpotResponse> findHotplacesByRegion(String region, int page, int size) {
-        Pageable pageable = PageRequest.of(
-                page, size, Sort.by(Sort.Direction.DESC, "viewCnt"));
-        Page<Spot> spots = spotRepository.findByRegionOrderByViewCntDesc(region, pageable);
-        return spots.getContent()
-                .stream().map(SpotResponse::new).collect(Collectors.toList());
+        Pageable pageable = PageRequest.of(page, size);
+        List<Spot> spots = spotRepository.findHotplacesByRegion(region, pageable);
+        return spots.stream().map(SpotResponse::new).collect(Collectors.toList());
+//        //총 페이지 수
+//        int totalPages = spots.getTotalPages();
+//        log.info("총 페이지 수: " + spots.getTotalPages());
+//
+//        //spotResponses DTO로 변환
+//        List<SpotResponse> spotResponses = spots.getContent()
+//                .stream().map(SpotResponse::new).collect(Collectors.toList());
+//
+//        // 총 페이지 수 추가하여 반환
+//        return SpotPageResponse.builder()
+//                .spotResponses(spotResponses)
+//                .totalPages(totalPages)
+//                .build();
     }
 
     // 태그별 핫플레이스 데이터 가져오기
-    public List<SpotResponse> findHotplaceByTag(int tagId, int page, int size) {
+    public SpotPageResponse findHotplacesByTag(int tagId, int page, int size) {
         Pageable pageable = PageRequest.of(page, size);
         Page<Spot> spots = spotRepository.findHotplaceByTag(tagId, pageable);
-        return spots.getContent()
+
+        //총 페이지 수
+        int totalPages = spots.getTotalPages();
+        log.info("총 페이지 수 - " + spots.getTotalPages());
+
+        //spotResponses DTO로 변환
+        List<SpotResponse> spotResponses = spots.getContent()
                 .stream().map(SpotResponse::new).collect(Collectors.toList());
+
+        // 총 페이지 수 추가하여 반환
+        return SpotPageResponse.builder()
+                .spotResponses(spotResponses)
+                .totalPages(totalPages)
+                .build();
     }
 
     // 상세 핫플레이스 데이터 가져오기
