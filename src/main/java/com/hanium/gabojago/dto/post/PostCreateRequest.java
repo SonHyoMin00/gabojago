79333edceedbc4e39c638e55f4dab27a877ed52d
권민정 <22alswjd@naver.com>
2022-08-05 04:38:38 +0900

    feat: 게시글 기능 - 전체 및 특정 게시글 조회, 게시글 작성

--- src/main/java/com/hanium/gabojago/dto/post/PostCreateRequest.java
+++ src/main/java/com/hanium/gabojago/dto/post/PostCreateRequest.java
@@ -1,4 +1,13 @@
 package com.hanium.gabojago.dto.post;
 
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@ToString
+@Getter @Setter
 public class PostCreateRequest {
+    private String email;
+    private String title;
+    private String context;
 }
--- src/main/java/com/hanium/gabojago/service/PostService.java
+++ src/main/java/com/hanium/gabojago/service/PostService.java
@@ -1,35 +1,82 @@
 package com.hanium.gabojago.service;
 
 import com.hanium.gabojago.domain.Post;
+import com.hanium.gabojago.domain.User;
 import com.hanium.gabojago.dto.post.PostCreateRequest;
 import com.hanium.gabojago.dto.post.PostPageResponse;
 import com.hanium.gabojago.dto.post.PostResponse;
 import com.hanium.gabojago.repository.PostRepository;
+import com.hanium.gabojago.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Slf4j
 @RequiredArgsConstructor
 @Service
 public class PostService {
     private final PostRepository postRepository;
+    private final UserRepository userRepository;
+
+    // Page<Post>를 PostPageResponse(dto)로 바꾸는 함수
+    // 계속 중복됨: 나중에 인터페이스로 나타내기
+    private PostPageResponse convertSPostsToPostPageResponse(Page<Post> posts) {
+        //총 페이지 수
+        int totalPages = posts.getTotalPages();
+        log.info("총 페이지 수: " + posts.getTotalPages());
+
+        //spotResponses DTO로 변환
+        List<PostResponse> postResponses = posts.getContent()
+                .stream().map(PostResponse::new).collect(Collectors.toList());
+
+        // 총 페이지 수 추가하여 반환
+        return PostPageResponse.builder()
+                .postResponses(postResponses)
+                .totalPages(totalPages)
+                .build();
+    }
 
     public PostPageResponse getPosts(int page, int size) {
-        return null;
+        Pageable pageable = PageRequest.of(page, size, Sort.by(Sort.Direction.DESC, "createdAt"));
+        Page<Post> posts = postRepository.findAllSpotsByPage(pageable);
+        return convertSPostsToPostPageResponse(posts);
     }
 
     @Transactional(readOnly = true)
     public PostResponse getPost(Long id) {
         Post post = postRepository.findById(id)
                 .orElseThrow(() -> new IllegalArgumentException("id " + id + "에 해당하는 게시글이 존재하지 않습니다."));
-        return null;
+        return PostResponse.builder()
+                .entity(post)
+                .build();
     }
 
+    @Transactional
     public Long createPost(PostCreateRequest postCreateRequest) {
-        return 0L;
+        String email = postCreateRequest.getEmail();
+        User user = userRepository.findByEmail(email)
+                .orElseThrow(()-> new IllegalArgumentException("이메일 \"" + email + "\"에 해당하는 사용자가 존재하지 않습니다."));
+
+        Post post = Post.builder()
+                .user(user)
+                .title(postCreateRequest.getTitle())
+                .context(postCreateRequest.getContext())
+                .build();
+
+        postRepository.save(post);
+
+        return post.getPostId();
     }
 
-    public Long updatePost(PostCreateRequest postCreateRequest) {
+    public Long updatePost(PostCreateRequest postCreateRequest, String email) {
         return 0L;
     }
 
