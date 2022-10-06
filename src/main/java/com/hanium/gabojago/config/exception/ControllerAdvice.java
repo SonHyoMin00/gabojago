5b255b9b4bbd36d4e975ea48017b57ea5e4e80b8
minpaeng <22alswjd@naver.com>
2022-10-06 17:55:45 +0900

    fix: 파일 첨부 용량 최대 50MB로 변경
    파일 첨부 용량 50MB로 설정, 예외처리 코드 추가

--- src/main/java/com/hanium/gabojago/config/exception/ControllerAdvice.java
+++ src/main/java/com/hanium/gabojago/config/exception/ControllerAdvice.java
@@ -2,10 +2,12 @@ package com.hanium.gabojago.config.exception;
 
 
 import lombok.extern.slf4j.Slf4j;
+import org.apache.tomcat.util.http.fileupload.impl.FileSizeLimitExceededException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.multipart.MultipartException;
 
 @Slf4j
 @RestControllerAdvice
@@ -15,6 +17,14 @@ public class ControllerAdvice {
     public ResponseEntity<String> runtimeException(RuntimeException e) {
         log.error(e.getMessage());
         return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(e.getMessage());
+    }
+
+    @ExceptionHandler(MultipartException.class)
+    public ResponseEntity<String> fileSizeLimitExceededException(MultipartException e) {
+        Throwable cause = e.getCause().getCause();
+        if(cause instanceof FileSizeLimitExceededException)
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("파일 업로드 용량을 초과했습니다. 제한된 용량: 50MB");
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("지원하지 않는 파일 첨부 요청입니다.");
     }
 
 }
